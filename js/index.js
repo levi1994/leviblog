af5ab3c2a9074201a04a03724735e706abc6377e
LeviLee <1219571443@qq.com>
2015-10-03 21:40:38 +0800

    the first version of leviblog
    update index.html and main html

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+	
+	$('#mainpage').fullpage({
+		sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90', '#4BBFC3'],
+		anchors: ['first','it', 'life', 'download', 'contact','other'],
+		menu: '#menu'
+	});
+
+    
+
+});
